739e62d1348b7baa36db6abe96bf1ae30e5d69d4
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2024-08-15 21:44:33 +0900

    オプション適用のコードを繰返し処理に。countメソッドを修正し入力形式を問わず処理を共通化。

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -2,70 +2,43 @@
 
 require 'optparse'
 
-opt = OptionParser.new
-params = {}
-opt.on('-l') { |v| params[:l] = v }
-opt.on('-w') { |v| params[:w] = v }
-opt.on('-c') { |v| params[:c] = v }
-opt.parse(ARGV)
-
 def count(information)
+  opt = OptionParser.new
+  params = {}
+  opt.on('-l') { |v| params[:l] = v }
+  opt.on('-w') { |v| params[:w] = v }
+  opt.on('-c') { |v| params[:c] = v }
+  opt.parse(ARGV)
   text_lines = information
   text = text_lines.join
-  @lines_count = text_lines.size
-  @words_count = text.split(/\s+/).size
-  @characters_count = text_lines.join.size
+  lines_count = text_lines.size
+  words_count = text.split(/\s+/).size
+  characters_count = text_lines.join.size
+  @output = []
+  params = { l: true, c: true, w: true } if params == {}
+  { l: lines_count, w: words_count, c: characters_count }.each do |key, value|
+    @output.push(output(value)) if params[key] == true
+  end
 end
 
 def output(element)
   element.to_s.rjust(8)
 end
 
-def params_true(array, element, option)
-  array.push(element) if option == true
-end
-
 if $stdin.isatty == false
   count($stdin.readlines)
-  if params[:l].nil? && params[:w].nil? && params[:c].nil?
-    output = [output(@lines_count), output(@words_count), output(@characters_count)]
-
-  else
-    output = []
-    params_true(output, output(@lines_count), params[:l])
-    params_true(output, output(@words_count), params[:w])
-    params_true(output, output(@characters_count), params[:c])
-  end
-  puts "#{output.join} "
+  puts "#{@output.join} "
 else
-  @output_l_sum = []
-  @output_w_sum = []
-  @output_c_sum = []
-  @output_params_sum = []
+  output_sum = []
   ARGV.each_with_index do |argument, idx|
     File.exist?(argument) ? file = argument : next
     count(File.readlines(file))
-    @output_params = []
-    if params[:l].nil? && params[:w].nil? && params[:c].nil?
-      output = [output(@lines_count), output(@words_count), output(@characters_count)]
-      @output_l_sum << @lines_count
-      @output_w_sum << @words_count
-      @output_c_sum << @characters_count
-      @output_sum = [output(@output_l_sum.sum), output(@output_w_sum.sum), output(@output_c_sum.sum)] if idx == ARGV.size - 1
-    else
-      params_true(@output_params, output(@lines_count), params[:l])
-      params_true(@output_params, output(@words_count), params[:w])
-      params_true(@output_params, output(@characters_count), params[:c])
-      @output_l_sum.push([@lines_count].sum) if params[:l] == true
-      @output_params_sum << (@output_l_sum.sum) if idx == ARGV.size - 1 && params[:l] == true
-      @output_w_sum.push([@words_count].sum) if params[:w] == true
-      @output_params_sum << @output_w_sum.sum if idx == ARGV.size - 1 && params[:w] == true
-      @output_c_sum.push([@characters_count].sum) if params[:c] == true
-      @output_params_sum << (@output_c_sum.sum) if idx == ARGV.size - 1 && params[:c] == true
-      @output_sum = @output_params_sum.map { |n| output(n).to_s }
+    puts "#{@output.join} #{argument}"
+    output_sum_element = @output.map { |n| n.delete(' ').to_i }
+    output_sum << output_sum_element
+    if idx == ARGV.size - 1
+      output_sum = output_sum.transpose.map { |n| output(n.inject(:+)) }
+      puts "#{output_sum.join} total"
     end
-    output = @output_params if !params[:l].nil? || !params[:w].nil? || !params[:c].nil?
-    puts "#{output.join} #{argument}"
-    puts "#{@output_sum.join} Total" if idx == ARGV.size - 1 && output != @output_sum
   end
 end
