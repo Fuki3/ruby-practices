fe6dca3704d39e834d989d0224393182dc342e02
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-03-18 23:37:30 +0900

    Shotのオブジェクトをオブジェクトのまま使うよう修正

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -4,24 +4,24 @@ require_relative 'shot'
 
 class Frame
   def initialize(frame)
-    @first_shot = Shot.new(frame[0]).convert_to_number
-    @second_shot = Shot.new(frame[1]).convert_to_number
-    @third_shot = Shot.new(frame[2]).convert_to_number
+    @first_shot = Shot.new(frame[0])
+    @second_shot = Shot.new(frame[1])
+    @third_shot = Shot.new(frame[2])
   end
 
   def caliculate_sum
     [
-      @first_shot,
-      @second_shot,
-      @third_shot
+      @first_shot.convert_to_number,
+      @second_shot.convert_to_number,
+      @third_shot.convert_to_number
     ].sum
   end
 
   def strike?
-    @first_shot == 10
+    @first_shot.convert_to_number == 10
   end
 
   def spare?
-    caliculate_sum == 10 && @first_shot != 10
+    caliculate_sum == 10 && @first_shot.convert_to_number != 10
   end
 end
