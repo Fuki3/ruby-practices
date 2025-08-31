1acb8a0fa84777d276228bbba73423ff2af688ff
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-08-31 21:19:58 +0900

    スペルミスの修正

--- 07.ls_object/long_formatter.rb
+++ 07.ls_object/long_formatter.rb
@@ -10,7 +10,7 @@ class LongFormatter
   def format
     file_details = @names.map { |name| FileDetail.new(name) }
     puts "total #{file_details.map(&:blocks).sum}"
-    max_size = culculate_max_size(file_details)
+    max_size = calcalate_max_size(file_details)
     file_details.each do |file_detail|
       name = file_detail.name
       nlink = file_detail.nlink.to_s.rjust(max_size[:nlink])
@@ -24,7 +24,7 @@ class LongFormatter
 
   private
 
-  def culculate_max_size(file_details)
+  def calcalate_max_size(file_details)
     %i[nlink owner group size].to_h do |key|
       [key, file_details.map { |file| file.send(key).to_s.size }.max]
     end
